32e70b6df97d8414dc2d5cb32a9931c960bbcc34
TowerChair <aaronstyle63@gmail.com>
2021-05-09 22:59:35 -0500

    avance

--- wish-list/src/app/app.module.ts
+++ wish-list/src/app/app.module.ts
@@ -3,12 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LetrasComponent } from './letras/letras.component';
+import { DestinoViajeComponent } from './destino-viaje/destino-viaje.component';
+import { ListaDestinosComponent } from './lista-destinos/lista-destinos.component';
+import { ModalComponent } from './modal/modal.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LetrasComponent
+    DestinoViajeComponent,
+    ListaDestinosComponent,
+    ModalComponent
   ],
   imports: [
     BrowserModule,
